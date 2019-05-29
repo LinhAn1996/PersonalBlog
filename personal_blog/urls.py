294c33870179a9663d11f9dc226bb4f79c6c39ef
linhanhoang1996@gmail.com <34492745+LinhAn1996@users.noreply.github.com>
2019-05-28 20:48:34 -0400

    Fix the template

--- personal_blog/urls.py
+++ personal_blog/urls.py
@@ -18,5 +18,6 @@ from django.urls import path, include
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('', include('blog.urls')),
+    path('', include('hello_world.urls')),
+    path('blog/', include('blog.urls')),
 ]
